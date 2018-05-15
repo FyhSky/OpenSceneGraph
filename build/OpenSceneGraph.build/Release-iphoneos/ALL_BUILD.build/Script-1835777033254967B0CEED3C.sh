3123fca5ab3c37143a61c53e152f21a310ed6586
冯英昊 <fengyinghao@qiyi.com>
2018-05-15 17:07:12 +0800

    no message

---
+++ build/OpenSceneGraph.build/Release-iphoneos/ALL_BUILD.build/Script-1835777033254967B0CEED3C.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /WorkSpace/osg-ios/OpenSceneGraph/build -f /WorkSpace/osg-ios/OpenSceneGraph/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ build/OpenSceneGraph.build/Release-iphoneos/ZERO_CHECK.build/Script-9CD0241481C041A6B5BFC298.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /WorkSpace/osg-ios/OpenSceneGraph/build -f /WorkSpace/osg-ios/OpenSceneGraph/build/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
